fd33e273bf13783ad42d63c48d584a1e63f37a5d
Max Sandoval <68491989+morpheomax@users.noreply.github.com>
2023-08-24 11:06:09 -0400

    Actualizacion User Controller

--- routes/User.routes.js
+++ routes/User.routes.js
@@ -14,6 +14,20 @@ const auth = require("../middlewares/auth");
 
 // Rutas de libre acceso CRUD
 router.get("/", getUsers);
+// Ruta para verificar si el correo electrónico ya está registrado
+router.get("/check-email/:email", async (req, res) => {
+  const { email } = req.params;
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(200).json({ exists: true });
+    }
+    return res.status(200).json({ exists: false });
+  } catch (error) {
+    console.error("Error al verificar el correo electrónico:", error);
+    return res.status(500).json({ exists: false });
+  }
+});
 router.post("/", signUp);
 router.put("/", updateUser);
 router.delete("/", deleteUser);
