7b3df35f59bd65e19ab1eca203eef1bb802b8cc3
Max Sandoval <68491989+morpheomax@users.noreply.github.com>
2023-09-02 12:21:46 -0400

    modificación de User Model

--- models/User.model.js
+++ models/User.model.js
@@ -66,7 +66,7 @@ const UserSchema = new mongoose.Schema(
     rol: {
       type: String,
       required: false,
-      default: "cliente",
+      default: "Cliente",
     },
     premium: {
       type: Boolean,
